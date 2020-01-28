0941f71c2bbf9640212457c9d5a78e7e0550a35a
slaterslater <slaterslater@gmail.com>
2020-01-28 08:42:12 -0500

    first commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,9 @@
+namespace GradeBook
+{
+  class Book
+  {
+
+  }
+
+
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var book = new Book();
+
+      List<double> grades = new List<double>() { 12.6454, 12.4767, 12.58787 };
+
+      var result = 0.0;
+      foreach (double number in grades)
+      {
+        result += number;
+      }
+      result /= grades.Count;
+      Console.WriteLine($"{result:N2}");
+    }
+  }
+}
